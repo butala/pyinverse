d4e7a0079ebe6e192850080927736d2b1858ab55
Mark D. Butala <butala@shaolin.attlocal.net>
2020-10-14 20:47:49 -0500

    First cut at ispectrum support.

--- pyinverse/axis.py
+++ pyinverse/axis.py
@@ -86,6 +86,21 @@ class RegularAxis:
         """
         return RegularAxis(self.x0, self.T/U, self.N*U)
 
+    def scale(self, S):
+        """ ??? """
+        print(self)
+        print(self._axis_t_T)
+        a = RegularAxis(self.x0*S, self.T*S, self.N)
+        a._axis_t_x0 = self._axis_t_x0
+        a._axis_t_N = self._axis_t_N
+        a._axis_t_T = self._axis_t_T
+        return a
+
+    def to_Hz(self):
+        """ ??? """
+        # Need to have units to know if this is valid
+        return self.scale(1 / (2*np.pi))
+
     def spectrum_axis(self, n, real=False):
         """ ??? """
         d = self.T/(2*np.pi)
@@ -95,6 +110,7 @@ class RegularAxis:
             axis_freq = FFTRegularAxis(n, d=d)
         axis_freq._axis_t_x0 = self.x0
         axis_freq._axis_t_N = self.N
+        axis_freq._axis_t_T = self.T ###
         return axis_freq
 
     def spectrum(self, x, n=None, real=False):
@@ -119,7 +135,31 @@ class RegularAxis:
 class FreqRegularAxis(RegularAxis):
     _axis_t_x0: float = None
     _axis_t_N: int = None
+    _axis_t_T: float = None  ###
     _N_FULL: int = None
+
+    @property
+    def d(self):
+        """ """
+        #return 1 / (self.T * self.N)
+        return 1 / (self.T * self._N_FULL)
+
+    def oversample(self, U):
+        """Return a new :class:`RegularAxis` with the same start point as
+        *regular* but with a factor *U* more sample points.
+
+        """
+        return type(self)(self._N_FULL*U, d=self.d)
+
+    def scale(self, S):
+        """ ??? """
+        a = type(self)(self._N_FULL, d=self.d/S)
+        a._axis_t_x0 = self._axis_t_x0
+        a._axis_t_N = self._axis_t_N
+        a._axis_t_T = self._axis_t_T
+        a._N_FULL = self._N_FULL
+        return a
+
 
     def ispectrum_axis(self, n):
         """ ??? """
@@ -167,12 +207,18 @@ class FFTRegularAxis(FreqRegularAxis):
         self._centers = scipy.fft.fftfreq(self.N, d=self._d)
         self._order = Order.FFT
 
+    def __post_init__(self):
+        """
+        """
+        pass
+
     def increasing(self, x=None):
         """
         """
         increasing_axis = RegularAxis(self.x0, self.T, self.N)
         increasing_axis._axis_t_x0 = self._axis_t_x0
         increasing_axis._axis_t_N = self._axis_t_N
+        increasing_axis._axis_t_T = self._axis_t_T ###
         if x is None:
             return increasing_axis
         else:
@@ -213,6 +259,8 @@ def dtft(x, real=False, n=None, n0=0):
     return RegularAxis(n0, 1, len(x)).spectrum(x, real=real, n=n)
 
 
+# ADD idft!!!
+
 
 @dataclass
 class RegularGrid:
@@ -252,6 +300,21 @@ class RegularGrid:
         """ ??? """
         # This also works for rfft?
         return self.axis_x.N_fast, self.axis_y.N_fast
+
+    def scale(self, Sx, Sy):
+        """ ??? """
+        return RegularGrid(self.axis_x.scale(Sx), self.axis_y.scale(Sy))
+
+    def to_Hz(self, axis=None):
+        """ ??? """
+        # Need to have units to know if this is valid
+        scale_map = {None: (1 / (2*np.pi), 1 / (2*np.pi)),
+                     0:    (1, 1 / (2*np.pi)),
+                     1:    (1 / (2*np.pi), 1)}
+
+        #return self.scale(1 / (2*np.pi), 1 / (2*np.pi))
+        return self.scale(*scale_map[axis])
+
 
     def increasing(self, x=None):
         """ ??? """
@@ -303,31 +366,34 @@ class RegularGrid:
 
     # y = rows, x = cols
     # s order is (y, x)
-    def spectrum_grid(self, s=None, axes=None, real=False):
+    def spectrum_grid(self, s=None, axis=None, real=False):
         """ ??? """
         if s is None:
             s = self.shape
-        AXES_MAP = {None: ('F', 'F'),
+        AXIS_MAP = {None: ('F', 'F'),
                     0:    ('F', 'S'),
                     1:    ('S', 'F')}
         if real:
-            AXES_MAP[None] = ('S', 'F')
+            AXIS_MAP[None] = ('F (real=False)', 'F')
 
         grid_freq_axis = []
-        for xform, n, axis in zip(AXES_MAP[axes], s, [self.axis_y, self.axis_x]):
+        for xform, n, self_axis in zip(AXIS_MAP[axis], s, [self.axis_y, self.axis_x]):
             if xform == 'F':
-                a = axis.spectrum_axis(n, real=real)
+                a = self_axis.spectrum_axis(n, real=real)
+            elif xform == 'F (real=False)':
+                a = self_axis.spectrum_axis(n, real=False)
             elif xform == 'S':
-                a = RegularAxis(axis.x0, axis.T/n, n)
+                a = RegularAxis(self_axis.x0, self_axis.T*self_axis.N/n, n)
             else:
                 assert False
-            a._axis_t_x0 = axis.x0
-            a._axis_t_N = axis.N
+            a._axis_t_x0 = self_axis.x0
+            a._axis_t_N = self_axis.N
+            a._axis_t_T = self_axis.T ###
             grid_freq_axis.append(a)
         return RegularGrid(grid_freq_axis[1], grid_freq_axis[0])
 
 
-    def spectrum(self, x, s=None, axes=None, real=False):
+    def spectrum(self, x, s=None, axis=None, real=False):
         """ ??? """
         assert x.shape == self.shape
         assert self.axis_x._order == Order.INCREASING and self.axis_y._order == Order.INCREASING
@@ -335,15 +401,63 @@ class RegularGrid:
             s = self.shape
         elif s < self.shape:
             raise NotImplementedError()
-        if real:
-            assert np.isrealobj(x)
-            X_spectrum = scipy.fft.rfft2(x, s=s, axes=axes)
-        else:
-            X_spectrum = scipy.fft.fft2(x, s=s, axes=axes)
-        grid_freq = self.spectrum_grid(s=s, axes=axes, real=real)
+        xform_map = {(True, None):  lambda x, s: scipy.fft.rfft2(x, s=s),
+                     (True, 0):     lambda x, s: scipy.fft.rfft(x, n=s[0], axis=0),
+                     (True, 1):     lambda x, s: scipy.fft.rfft(x, n=s[1], axis=1),
+                     (False, None): lambda x, s: scipy.fft.fft2(x, s=s),
+                     (False, 0):    lambda x, s: scipy.fft.fft(x, n=s[0], axis=0),
+                     (False, 1):    lambda x, s: scipy.fft.fft(x, n=s[1], axis=1)}
+        X_spectrum = xform_map[(real, axis)](x, s)
+        grid_freq = self.spectrum_grid(s=s, axis=axis, real=real)
         P = np.exp(-1j*(grid_freq.centers[0]*self.axis_x.x0 + grid_freq.centers[1]*self.axis_y.x0))
         X_spectrum *= P * self.axis_x.T * self.axis_y.T
         return grid_freq, X_spectrum
+
+
+    def ispectrum_grid(self, s=None, axes=None):
+        """ ??? """
+        if s is None:
+            s = self.shape
+        # AXES_MAP = {None: ('F', 'F'),
+        #             0:    ('F', 'S'),
+        #             1:    ('S', 'F')}
+        AXES_MAP = {None: ('S', 'S')}
+        # if real:
+            # AXES_MAP[None] = ('S', 'S')
+
+        grid_axis = []
+        for xform, n, axis in zip(AXES_MAP[axes], s, [self.axis_y, self.axis_x]):
+            if xform == 'S':
+                if axis._order != Order.FFT:
+                    assert isinstance(axis, RFFTRegularAxis)
+                a = axis.ispectrum_axis(n)
+            else:
+                assert False
+            a._axis_t_x0 = axis.x0
+            a._axis_t_N = axis.N
+            a._axis_t_T = axis.T ###
+            grid_axis.append(a)
+        return RegularGrid(grid_axis[1], grid_axis[0])
+
+
+    def ispectrum(self, X_spectrum, s=None, axes=None):
+        """ ??? """
+        assert X_spectrum.shape == self.shape
+        if s is None:
+            #s = self.shape
+            s = (self.axis_x._axis_t_N, self.axis_y._axis_t_N)
+        elif s < self.shape:
+            raise NotImplementedError()
+        grid = self.ispectrum_grid(s=s, axes=axes)
+        P = np.exp(1j*(self.centers[0]*self.axis_x._axis_t_x0 + self.centers[1]*self.axis_y._axis_t_x0))
+        assert axes is None
+        if isinstance(self.axis_x, FFTRegularAxis) and isinstance(self.axis_y, FFTRegularAxis):
+            x = scipy.fft.ifft2(X_spectrum * P / (grid.axis_x.T * grid.axis_y.T), s=s)
+        elif isinstance(self.axis_x, RFFTRegularAxis) and isinstance(self.axis_y, FFTRegularAxis):
+            x = scipy.fft.irfft2(X_spectrum * P / (grid.axis_x.T * grid.axis_y.T), s=s)
+        else:
+            assert False
+        return grid, x
 
 
 def dtft2(x, real=False, s=None, n0=(0, 0)):
@@ -354,3 +468,14 @@ def dtft2(x, real=False, s=None, n0=(0, 0)):
     axis_y = RegularAxis(n0[0], 1, x.shape[0])
     grid = RegularGrid(axis_x, axis_y)
     return grid.spectrum(x, real=real, s=s)
+
+
+# def idtft2(x_dtft2, real=False, s=None, n0=(0, 0)):
+#     """ ??? """
+#     if s is None:
+#         s = x_dtft2.shape
+#     axis_x = RegularAxis(n0[1], 1, x_dtft2.shape[1])
+#     axis_y = RegularAxis(n0[0], 1, x_dtft2.shape[0])
+#     grid = RegularGrid(axis_x, axis_y)
+#     grid_spectrum = grid.spectrum_grid(s=s, real=real)
+#     return grid_spectrum.ispectrum(x_dtft2, real=real, s=s)
