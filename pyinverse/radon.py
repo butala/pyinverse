027ec25aef59228debe7a5c8934cea8ca89ba9f9
Mark D. Butala <butala@shaolin-2.local>
2022-11-01 20:19:14 +0000

    Added polytope area calculation capability.

--- pyinverse/radon.py
+++ pyinverse/radon.py
@@ -11,6 +11,7 @@ import scipy.sparse
 from .axis import RegularAxis
 from .grid import RegularGrid
 from .rect import srect_2D_proj, rect_conv_radon_rect
+from .volume import volume_cal
 
 
 def radon_translate(theta_rad, r, x0, y0):
@@ -51,7 +52,89 @@ def radon_affine_scale(theta_rad, r, alpha, beta):
     return theta_prime, r_prime, scale_factor
 
 
-def radon_matrix_ij(grid, grid_y, ij, a=0):
+def regular_grid2polytope(grid, ij):
+    """Return the polytope, i.e., A and b in the equation Ax <= b,
+    that corresponds to (ij)th grid element."""
+    A = [[-1,  0],
+         [ 1,  0],
+         [ 0, -1],
+         [ 0,  1]]
+    i, j = ij
+    b = [-grid.axis_y.borders[i],
+          grid.axis_y.borders[i + 1],
+         -grid.axis_x.borders[j],
+          grid.axis_x.borders[j + 1]]
+    return A, b
+
+
+def theta_grid2half_planes(grid_y, kl, rad=False):
+    """Return the polytope, i.e., A and b in the equation Ax <= b,
+    that corresponds to the strip of integration for the (*kl*)th
+    element in the Radon transform where k is the theta index and l is
+    the projection axis coordinate index which are specified by
+    *grid_y*. If *rad*, then angular coordinates are given in radians
+    as opposed to degrees.
+    """
+    k, l = kl
+    theta_k = grid_y.axis_x.centers[k]
+    if not rad:
+        theta_k = np.radians(theta_k)
+    theta_k %= 2*np.pi
+    if theta_k == 0:
+        A = [[0, -1],
+             [0,  1]]
+        b = [-grid_y.axis_y.borders[l],
+              grid_y.axis_y.borders[l+1]]
+    elif theta_k == np.pi:
+        A = [[0, -1],
+             [0,  1]]
+        b = [-grid_y.axis_y.borders[-(l+2)],
+              grid_y.axis_y.borders[-(l+1)]]
+    elif theta_k == np.pi/2:
+        A = [[-1, 0],
+             [ 1, 0]]
+        b = [-grid_y.axis_y.borders[l],
+              grid_y.axis_y.borders[l+1]]
+    elif theta_k == 3*np.pi/2:
+        A = [[-1, 0],
+             [ 1, 0]]
+        b = [-grid_y.axis_y.borders[-(l+2)],
+              grid_y.axis_y.borders[-(l+1)]]
+    else:
+        c_k = np.cos(theta_k)
+        s_k = np.sin(theta_k)
+        y1, x1 = np.array([s_k*grid_y.axis_y.borders[l], c_k*grid_y.axis_y.borders[l]])
+        y2, x2 = np.array([s_k*grid_y.axis_y.borders[l+1], c_k*grid_y.axis_y.borders[l+1]])
+        A = np.array([[-s_k, -c_k],
+                      [ s_k,  c_k]])
+        b = np.array([-y1*s_k - x1*c_k,
+                       y2*s_k + x2*c_k])
+    return A, b
+
+
+def radon_matrix_ij_polytope(grid, grid_y, ij, a=0, rad=False):
+    """
+    """
+    i, j = ij
+    Np, Na = grid_y.shape
+
+    data = []
+    indices = []
+
+    for l in range(Np):
+        for k in range(Na):
+            A_t, b_t = theta_grid2half_planes(grid_y, (k, l), rad=rad)
+            A_grid, b_grid = regular_grid2polytope(grid, (i, j))
+            A_lass = np.vstack((A_grid, A_t))
+            b_lass = np.hstack((b_grid, b_t))
+            p_theta_lk = volume_cal(6, 2, A_lass, b_lass) / grid_y.axis_y.T
+            if not np.allclose(p_theta_lk, 0):
+                data.append(p_theta_lk)
+                indices.append(l * Na + k)
+    return data, indices
+
+
+def radon_matrix_ij_analytic(grid, grid_y, ij, a=0):
     """
     """
     data = []
@@ -75,15 +158,16 @@ def radon_matrix_ij(grid, grid_y, ij, a=0):
             I_nz = np.nonzero(p_theta_k[:, 0])[0]
             data_k = p_theta_k[I_nz, 0]
         else:
-            p_theta_k = rect_conv_radon_rect(theta_k, t_prime, Tx, Ty, 1/a)
+            p_theta_k = rect_conv_radon_rect(theta_k, t_prime, Tx, Ty, 1/a) * a
             I_nz = np.nonzero(p_theta_k)[0]
-            data_k = p_theta_k[I_nz] * a
+            data_k = p_theta_k[I_nz]
         data.extend(data_k)
         indices.extend(I_nz * Na + k)
     return data, indices
 
 
-def radon_matrix(grid, grid_y, a=0, n_cpu=multiprocessing.cpu_count(), chunksize=8):
+def radon_matrix(grid, grid_y, a=0, n_cpu=multiprocessing.cpu_count(), chunksize=8,
+                 _radon_matrix_ij=radon_matrix_ij_analytic):
     """Calculate the matrix form of the Radon transform for an object
     specified on *grid* and projections defined on *grid_y*. The
     parameter *a* specifies the beam width (rect integration applied
@@ -103,7 +187,7 @@ def radon_matrix(grid, grid_y, a=0, n_cpu=multiprocessing.cpu_count(), chunksize
 
     ij = product(range(Ny), range(Nx))
 
-    radon_matrix_helper = partial(radon_matrix_ij, grid, grid_y, a=a)
+    radon_matrix_helper = partial(_radon_matrix_ij, grid, grid_y, a=a)
 
     with multiprocessing.Pool(n_cpu) as pool:
         for data_ij, indices_ij in tqdm(pool.imap(radon_matrix_helper, ij, chunksize), total=Nx*Ny):
