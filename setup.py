7678dc4b89553cc8ecdaba713808db50afddfd65
Mark D. Butala <butala@vpnpool-10-251-87-82.near.illinois.edu>
2023-05-20 17:09:09 +0800

    Using the new way of specifying Python project files.

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 from setuptools import setup, Extension
 
 
-setup(name='pyinverse',
-      version='0.1' ,
-      description='Inverse problem tools for python.',
-      author='Mark D. Butala',
-      author_email='butala@illinois.edu',
-      packages=['pyinverse'],
-      ext_modules=[
+setup(ext_modules=[
           Extension(name='lassere',
-                    sources=['./lassere/lassere.c',
-                             './lassere/util.c'],
-                    extra_compile_args=['-O3'])],
-      zip_safe=False
+                    sources=['pyinverse/lassere/lassere.c',
+                             'pyinverse/lassere/util.c'],
+                    extra_compile_args=['-O3'])]
 )
